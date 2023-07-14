14fb5edd4b7443d27a707fdfa05318cef367d4ca
NitaRose <rose_nita@outlook.com>
2023-07-13 20:03:36 -0400

    Update RPSLS/Game.cs

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,20 @@ namespace RPSLS
     internal class Game
     {
         //Member Variabes (HAS A)
-        public Player playerOne;
-        public Player playerTwo;
-
+        public Player PlayerOne(string name);
+        public Player PlayerTwo(string name);
+        
+        int playerOneScore;
+        int playerTwoScore;
+        int mainBrainScore;
+        
         //Constructor
         public Game()
         {
+            Player player = new Player(playerOne);
+            Player players = new Player(playerTwo);
+            int playerOneScore = new int();
+             
 
         }
 
@@ -24,13 +33,14 @@ namespace RPSLS
             Console.WriteLine("Welcome to RPSLS! Here are the rules:\n");
             Console.WriteLine("Rock crushes Scissors\nScissors cuts Paper \nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\n");
             Console.WriteLine("How many Brave souls will tread this journey today?");
-            Console.ReadLine();
-            Console.WriteLine("Player One please enter your name: ");
-            Console.ReadLine();
+            int numberOfHumanPlayers = int.Parse(Console.ReadLine());
+
         }
 
         public int ChooseNumberOfHumanPlayers()
-        {
+        {       //IF statement 
+
+            
             return 0;
         }
 
